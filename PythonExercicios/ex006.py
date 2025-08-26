9f528ce19a4080973d5b204e9b436cd8b21d7c5b
Renato Abuchaim <renato_pacheco_@hotmail.com>
2025-08-26 00:37:29 -0300

    Ex006

---
+++ PythonExercicios/ex006.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um número: '))
+#dobro = n * 2
+#triplo = n * 3
+#raiz = n ** (1/2)
+#print('O dobro de {} vale {}.\nO triplo de {} vale {}.\nRaiz quadrada de {} é igual a {:.2f}'.format(n, dobro, n, triplo, n, raiz))
+
+print('O dobro de {} vale {}.\nO triplo de {} vale {}.\nRaiz quadrada de {} é igual a {:.2f}'.format(n, (n*2), n, (n*3), n, (n**(1/2))))
