b19cac065fafb7c5a39a9c9b61e88fc93ed7ef25
Renato Abuchaim <renato_pacheco_@hotmail.com>
2025-08-26 00:09:10 -0300

    Teste 003

--- script-python/teste003.py
+++ script-python/teste003.py
@@ -1,1 +1,9 @@
-a = int(input(''))
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma é {}, o produto é {} e a divisão é {:.3f}'.format(s, m, d), end=' ')
+print('Divisão inteira {} e a potência {}'.format(di, e))
