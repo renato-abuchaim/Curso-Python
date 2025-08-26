293a81dea975e131c42db6b1d689f0f3bc89c40a
Renato Abuchaim <renato_pacheco_@hotmail.com>
2025-08-26 00:18:05 -0300

    Ex005

---
+++ PythonExercicios/ex005.py
@@ -0,0 +1,7 @@
+n = int(input('Digiti um número: '))
+
+#antecessor = n - 1
+#sucessor = n + 1
+#print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, antecessor, sucessor))
+
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, (n-1), (n+1)))
